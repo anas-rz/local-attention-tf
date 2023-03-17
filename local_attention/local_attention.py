3bbf99ac7a8445e30b554324f0741d833719049f
Muhammad Anas Raza <63569834+anas-rz@users.noreply.github.com>
2023-03-16 23:08:45 -0400

    Update local_attention.py

--- local_attention/local_attention.py
+++ local_attention/local_attention.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 import tensorflow.keras.backend as K
 import math
+from tensorflow.keras import layers
+from einops import rearrange, pack, unpack
+
+
+
 
 def exists(val):
     return val is not None
@@ -36,10 +41,88 @@ def pad_sequence_to_multiple_of(seq, multiple, axis):
 def look_around(x, backward=1, forward=0, pad_value=-1, axis=2):
     t = K.int_shape(x)[1]
     tensor_rank = tf.rank(x)
-    f_i = len(K.int_shape(x)) - axis
+    f_i = len(K.int_shape(x)) - axis - 1
     padding_tensor = tf.zeros((tensor_rank, 2), dtype=tf.int32)
     padding_tensor = tf.tensor_scatter_nd_update(padding_tensor, [[f_i, 0], [f_i, 1]], [backward, forward])
     padded_x = tf.pad(x, padding_tensor, constant_values=pad_value)
-    print(padded_x.shape)
     tensors = [padded_x[:, ind:(ind + t), ...] for ind in range(forward + backward + 1)]
     return tf.concat(tensors, axis=axis)
+
+class LocalAttention(layers.Layer):
+    def __init__(
+        self,
+        window_size,
+        look_backward = 1,
+        look_forward = None,
+        dropout = 0.,
+        exact_windowsize = False,
+        scale = None,
+    ):
+        super().__init__()
+        look_forward = 0
+
+        self.scale = scale
+
+        self.window_size = window_size
+        self.exact_windowsize = exact_windowsize
+
+
+        self.look_backward = look_backward
+        self.look_forward = look_forward
+
+        self.dropout = layers.Dropout(dropout)
+
+
+    def call(
+        self,
+        q, k, v,
+        window_size = None
+    ):
+
+
+
+        shape, pad_value, window_size,  look_backward, look_forward = q.shape, -1, default(window_size, self.window_size), self.look_backward, self.look_forward
+
+        # https://github.com/arogozhnikov/einops/blob/master/docs/4-pack-and-unpack.ipynb
+        (q, packed_shape), (k, _), (v, _) = map(lambda t: pack([t], '* n d'), (q, k, v))
+
+        # auto padding
+
+        
+        b, n, dim_head, dtype = *K.int_shape(q), q.dtype
+
+        scale = default(self.scale, dim_head ** -0.5)
+
+        assert (n % window_size) == 0, f'sequence length {n} must be divisible by window size {window_size} for local attention'
+
+        windows = n // window_size
+
+
+        # bucketing
+
+        bq, bk, bv = map(lambda t: rearrange(t, 'b (w n) d -> b w n d', w = windows), (q, k, v))
+
+        bq = bq * scale
+
+        look_around_kwargs = dict(
+            backward =  look_backward,
+            forward =  look_forward,
+            pad_value = pad_value
+        )
+
+        bk = look_around(bk, **look_around_kwargs)
+        bv = look_around(bv, **look_around_kwargs)
+
+        sim = tf.einsum('b h i e, b h j e -> b h i j', bq, bk)
+        # attention
+
+        attn = tf.nn.softmax(sim, axis=-1)
+        attn = self.dropout(attn)
+
+        # aggregation
+
+        out = tf.einsum('b h i j, b h j e -> b h i e', attn, bv)
+        out = rearrange(out, 'b w n d -> b (w n) d')
+
+        out, *_ = unpack(out, packed_shape, '* n d')
+        return out
