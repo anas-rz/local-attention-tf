468608de06b4395b53c6dfcfbdd01f73b6f5db13
Muhammad Anas Raza <63569834+anas-rz@users.noreply.github.com>
2023-03-18 13:08:40 -0400

    Added Transformer layer

--- local_attention/transformer.py
+++ local_attention/transformer.py
@@ -1,25 +1,67 @@
 from tensorflow.keras import layers
-from tensorflow import keras
 
-def LocalMHA( input_shape,
+class LocalMHA(layers.Layer):
+    def __init__(
+        self,
         window_size,
         dim_head = 64,
         heads = 8,
         dropout = 0.,
+        causal = False,
         prenorm = False,
         qk_rmsnorm = False,
         qk_scale = 8,
-        use_xpos = False,
-        xpos_scale_base = None):
-    dim = input_shape[-1]
-    inner_dim = dim_head * heads
-    i_p = layers.Input(shape=input_shape)
-    if prenorm:
-        i_p = layers.LayerNormalization()(i_p)
-    x = layers.Dense(inner_dim * 3) (i_p)
-    q, k, v = layers.Lambda(lambda _x: tf.split(_x, 3, axis=-1))(x)
-    q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = heads), (q, k, v))
-    attn = LocalAttention(window_size)(q, k, v)
-    # attn = rearrange(attn, 'b h n d -> b n (h d)')
-    out = layers.Dense(dim)(attn)
-    return keras.Model(i_p, out)
+        # use_xpos = False,
+        xpos_scale_base = None,
+        **kwargs
+    ):
+        super().__init__()        
+        self.inner_dim = dim_head * heads
+        self.window_size = window_size
+        self.causal = causal
+
+        self.heads = heads
+        self.qk_rmsnorm = qk_rmsnorm
+        self.prenorm = prenorm
+
+    def build(self, input_shape):
+        dim = input_shape[-1]
+        self.norm = layers.LayerNormalization(axis=-1) if self.prenorm else None
+
+        self.to_qkv = layers.Dense(self.inner_dim * 3, use_bias = False)
+
+
+        # if self.qk_rmsnorm:
+        #     self.q_scale = nn.Parameter(torch.ones(dim_head))
+        #     self.k_scale = nn.Parameter(torch.ones(dim_head))
+
+        self.attn_fn = LocalAttention(
+            window_size = self.window_size,
+            causal = self.causal,
+            autopad = True,
+            scale = (self.qk_scale if self.qk_rmsnorm else None),
+            exact_windowsize = True,
+            # use_xpos = self.use_xpos,
+            # xpos_scale_base = self.xpos_scale_base,
+            # **kwargs
+        )
+
+        self.to_out = layers.Dense(dim, use_bias = False)
+
+    def call(self, x, mask = None, attn_bias = None):
+        if exists(self.norm):
+            x = self.norm(x)
+
+        q, k, v = tf.split(self.to_qkv(x),3, axis=-1)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), (q, k, v)) 
+
+        # if self.qk_rmsnorm:
+        #     q, k = map(l2norm, (q, k))
+        #     q = q * self.q_scale
+        #     k = k * self.k_scale
+
+        out = self.attn_fn(q, k, v, mask = mask)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
