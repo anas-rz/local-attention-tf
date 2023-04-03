3bd250d580da64cb7d96011b8c7fddb83067c8cc
Muhammad Anas Raza <memanasraza@gmail.com>
2023-04-03 01:40:35 -0400

    Added Doc

--- local_attention_tf/blocks.py
+++ local_attention_tf/blocks.py
@@ -3,6 +3,18 @@ from tensorflow.keras import layers
 from .layers import GEGLU, LocalMHA
 # Define FeedForward layer
 def FeedForward(dim, mult=4, dropout=0.):
+    """
+    A feedforward layer composed of a dense layer with gated linear unit (GLU) activation, dropout, and layer normalization.
+    
+    Args:
+        dim: An integer representing the dimensionality of the input and output tensors.
+        mult: A float representing the multiplier to compute the inner dimension of the dense layer. Defaults to 4.
+        dropout: A float representing the dropout rate. Defaults to 0.
+        
+    Returns:
+        A Keras Sequential model composed of a layer normalization layer, a dense layer with GLU activation, dropout, and a dense layer.
+    """
+
     inner_dim = int(dim * mult * 2 / 3)
 
     return tf.keras.Sequential([
@@ -25,6 +37,24 @@ def LocalTransformer(
     ff_dropout=0.0,
     **kwargs
 ):
+    """
+    A function that returns a callable that applies a local transformer on the input tensor.
+
+    Args:
+        dim: An integer, the dimension of the input tensor.
+        depth: An integer, the depth of the local transformer.
+        out_dim: An integer, the dimension of the output tensor. Defaults to dim.
+        local_attn_window_size: An integer, the size of the local attention window. Defaults to 512.
+        dim_head: An integer, the dimension of each attention head. Defaults to 64.
+        heads: An integer, the number of attention heads. Defaults to 8.
+        ff_mult: An integer, the multiplier for the hidden dimension of the feedforward network. Defaults to 4.
+        attn_dropout: A float, the dropout rate for the attention layer. Defaults to 0.0.
+        ff_dropout: A float, the dropout rate for the feedforward layer. Defaults to 0.0.
+        **kwargs: Additional keyword arguments passed to the LocalMHA layer.
+
+    Returns:
+        A callable that applies a local transformer on the input tensor.
+    """
     if not out_dim:
         out_dim = dim
     def _apply(x):
--- local_attention_tf/layers.py
+++ local_attention_tf/layers.py
@@ -4,13 +4,47 @@ from einops import rearrange
 import tensorflow.keras.backend as K
 
 def default(value, d):
+    """
+    The default function takes two arguments: value and d. If the value is None, it returns the default value d, otherwise it returns the value.
+
+    Parameters:
+
+        value: A value to check if it is None or not.
+        d: The default value that will be returned if the value is None.
+    Returns:
+
+        The value if it is not None, otherwise the default value d.
+
+    """
     return d if not exists(value) else value
 
 def exists(val):
+    """
+    Checks if a value/module exists.
+
+    Args:
+        val: The value/module to check.
+
+    Returns:
+        bool: True if the value/module exists, False otherwise.
+    """
     return val is not None
 
 @tf.function
 def look_around(x, backward=1, forward=0, pad_value=-1, axis=2):
+    """
+       Applies a sliding window to a tensor along a given axis, and concatenates the resulting tensors.
+
+    Args:
+        x: The input tensor.
+        backward (int): The number of indices to look backward.
+        forward (int): The number of indices to look forward.
+        pad_value (int): The padding value to use.
+        axis (int): The axis to apply the sliding window along.
+
+    Returns:
+        tf.Tensor: The resulting tensor after applying the sliding window and concatenation.
+    """
     t = K.int_shape(x)[1]
     tensor_rank = tf.rank(x)
     padding_tensor = tf.zeros((tensor_rank, 2), dtype=tf.int32)
@@ -22,6 +56,21 @@ def look_around(x, backward=1, forward=0, pad_value=-1, axis=2):
 
 
 class LocalAttention(layers.Layer):
+    """
+    performs local attention on input tensors q, k, and v.
+
+    Args:
+        window_size (int): The size of the attention window.
+        look_backward (int, optional): The number of previous elements to include in the attention window. Defaults to 1.
+        look_forward (int, optional): The number of subsequent elements to include in the attention window. Defaults to None.
+        dropout (float, optional): The dropout rate to apply to the attention scores. Defaults to 0.
+
+    Methods:
+        call(q, k, v, window_size=None): Performs the local attention operation on the input tensors q, k, and v.
+
+    Returns:
+        tf.Tensor: The output tensor after the local attention operation has been performed.
+    """
     def __init__(
         self,
         window_size,
@@ -73,6 +122,24 @@ class LocalAttention(layers.Layer):
         return out
 
 class LocalMHA(layers.Layer):
+    """
+    The LocalMHA layer uses a dense linear projection to map the input tensor to separate query, 
+    key, and value tensors for each head, which are then passed to the LocalAttention layer to 
+    compute the attention scores. The output of the local attention operation is then 
+    concatenated across the heads dimension and projected back to the original tensor 
+    dimension using another dense layer. 
+
+    Args:
+        window_size (int): The size of the local window to apply attention to.
+        dim_head (int): The size of each attention head.
+        heads (int): The number of attention heads to use.
+        dropout (float): The dropout rate to apply to the attention scores.
+        prenorm (bool): Whether to apply layer normalization before the attention operation.
+        qk_scale (float): The scaling factor to apply to the dot product of the query and key tensors.
+
+    Returns:
+        tf.Tensor: The output of the LocalMHA layer, with the same shape as the input tensor.
+    """
     def __init__(
         self,
         window_size,
@@ -119,6 +186,19 @@ class LocalMHA(layers.Layer):
 
 class TokenAndPositionEmbedding(layers.Layer):
     # https://keras.io/examples/nlp/text_classification_with_transformer/
+    """
+    This layer combines token and position embeddings for input sequences. It takes in a maximum sequence length,
+    vocabulary size, and embedding dimension as arguments. It uses two separate embedding layers - one for token
+    embeddings and the other for position embeddings. The position embedding is added element-wise to the token
+    embedding to create the final embedding.
+
+    Attributes:
+        token_emb (tf.keras.layers.Embedding): Embedding layer for token embeddings.
+        pos_emb (tf.keras.layers.Embedding): Embedding layer for position embeddings.
+
+    Methods:
+        call(x): Takes in an input tensor x and returns the sum of token and position embeddings.
+    """
     def __init__(self, maxlen, vocab_size, embed_dim):
         super().__init__()
         self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
@@ -133,6 +213,18 @@ class TokenAndPositionEmbedding(layers.Layer):
 
 # Define GEGLU layer
 class GEGLU(tf.keras.layers.Layer):
+    """
+    A gated linear unit with exponential linear unit (GEGLU) activation function. This layer splits the input tensor into two halves along the last dimension, applies the gelu activation to the second half, and multiplies the first half by the gelu output.
+
+Arguments:
+    None
+
+Call arguments:
+    x: Input tensor of shape `(batch_size, ..., input_dim)`. The last dimension must be even.
+
+Returns:
+    The GEGLU activation of the input tensor, of shape `(batch_size, ..., input_dim // 2)`.
+    """
     def call(self, x):
         x, gate = tf.split(x, num_or_size_splits=2, axis=-1)
         return x * tf.nn.gelu(gate)
